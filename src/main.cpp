273c54a1f8de27db5a1030c31ee3a9fbccbfc4e6
brunocmo <brunocmo.nunes@gmail.com>
2022-03-04 23:17:42 +0000

    teste lcd

--- src/main.cpp
+++ src/main.cpp
@@ -20,76 +20,76 @@ int main() {
 	CommsProtocol * uart = new CommsProtocol();
 	ControleTemperatura controleDaTemperatura;
 
-	char sistemaTelaAcima[16] = "";
-	char sistemaTelaAbaixo[16] = "";
+	// char sistemaTelaAcima[16] = "";
+	// char sistemaTelaAbaixo[16] = "";
 
-	char sistemaDesligadoAcima[16] = "   Desligado   ";
-	char sistemaDesligadoAbaixo[16] = "               ";
+	// char sistemaDesligadoAcima[16] = "   Desligado   ";
+	// char sistemaDesligadoAbaixo[16] = "               ";
 
-	int sinalControle{0};
-	bool sistemaLigado{false};
-	signal(SIGINT, tratarSinal);
+	// int sinalControle{0};
+	// bool sistemaLigado{false};
+	// signal(SIGINT, tratarSinal);
 
-	pid_configura_constantes( 30.0, 0.2, 400.0 );
+	// pid_configura_constantes( 30.0, 0.2, 400.0 );
 
-	uart->enviarSinalDeReferencia( 28.00f );
+	// uart->enviarSinalDeReferencia( 28.00f );
 
-	while(executar) {
+	// while(executar) {
 
-		uart->lerComandosDoUsuario();
+	// 	uart->lerComandosDoUsuario();
 
-		switch( uart->get_codigoRetorno() ) {
-			case 1:
-				uart->enviarDisplayEstadoSistema( 0x01 );
-				sistemaLigado = true;
-				break;
-			case 2: 
-				uart->enviarDisplayEstadoSistema( 0x00 );
-				lcd->set_mensagemAcima16(sistemaDesligadoAcima);
-				lcd->set_mensagemAbaixo16(sistemaDesligadoAbaixo);
-				lcd->mostrarMensagem();
-				sistemaLigado = false;
-				break;
-			case 3:
-				uart->enviarDisplayControle( 0x00 );
-				break;
-			case 4:
-				uart->enviarDisplayControle( 0x01 );
-			default: break;	
-		}
+	// 	switch( uart->get_codigoRetorno() ) {
+	// 		case 1:
+	// 			uart->enviarDisplayEstadoSistema( 0x01 );
+	// 			sistemaLigado = true;
+	// 			break;
+	// 		case 2: 
+	// 			uart->enviarDisplayEstadoSistema( 0x00 );
+	// 			lcd->set_mensagemAcima16(sistemaDesligadoAcima);
+	// 			lcd->set_mensagemAbaixo16(sistemaDesligadoAbaixo);
+	// 			lcd->mostrarMensagem();
+	// 			sistemaLigado = false;
+	// 			break;
+	// 		case 3:
+	// 			uart->enviarDisplayControle( 0x00 );
+	// 			break;
+	// 		case 4:
+	// 			uart->enviarDisplayControle( 0x01 );
+	// 		default: break;	
+	// 	}
 
-		if( sistemaLigado  ) {
-			uart->solicitarTemperaturaInterna();
-			uart->solicitarTemperaturaPotenciometro();
+	// 	if( sistemaLigado  ) {
+	// 		uart->solicitarTemperaturaInterna();
+	// 		uart->solicitarTemperaturaPotenciometro();
 
-			pid_atualiza_referencia( uart->get_temperaturaReferencia() );
-			sinalControle = (int)pid_controle( uart->get_temperaturaInterna() );
+	// 		pid_atualiza_referencia( uart->get_temperaturaReferencia() );
+	// 		sinalControle = (int)pid_controle( uart->get_temperaturaInterna() );
 
-			uart->enviarSinalDeControle( sinalControle );
+	// 		uart->enviarSinalDeControle( sinalControle );
 
-			controleDaTemperatura.mudarTemperatura(sinalControle);
+	// 		controleDaTemperatura.mudarTemperatura(sinalControle);
 
-			sprintf( sistemaTelaAcima, "TR %.2f TE %.2f", uart->get_temperaturaReferencia(), tempAmbiente->get_temperatura());
-			sprintf( sistemaTelaAbaixo, "TI %.2f        ", uart->get_temperaturaInterna());
+	// 		sprintf( sistemaTelaAcima, "TR %.2f TE %.2f", uart->get_temperaturaReferencia(), tempAmbiente->get_temperatura());
+	// 		sprintf( sistemaTelaAbaixo, "TI %.2f        ", uart->get_temperaturaInterna());
 
-			lcd->set_mensagemAcima16( sistemaTelaAcima);
-			lcd->set_mensagemAbaixo16(sistemaTelaAbaixo);
-			lcd->mostrarMensagem();
-		}
-	}
+	// 		lcd->set_mensagemAcima16( sistemaTelaAcima);
+	// 		lcd->set_mensagemAbaixo16(sistemaTelaAbaixo);
+	// 		lcd->mostrarMensagem();
+	// 	}
+	// }
 
 
 	char temperaturaAmbiente[16] = "Temp. Ambiente:";
 	char tempReferencia[16] = "Temp. Pot.:";
-	// char tempInterna[16] = "Temp. Interna.:";
+	char tempInterna[16] = "Temp. Interna.:";
 
 
-	// teste->solicitarTemperaturaInterna();
-	// teste->solicitarTemperaturaPotenciometro();
+	uart->solicitarTemperaturaInterna();
+	uart->solicitarTemperaturaPotenciometro();
 
-	// printf("A temperatura ambiente   eh: %.4lf\n", statusTemperatura->get_temperatura());
-	// printf("A temperatura referencia eh: %.4lf\n", teste->get_temperaturaReferencia());
-	// printf("A temperatura interna    eh: %.4lf\n", teste->get_temperaturaInterna());
+	printf("A temperatura ambiente   eh: %.4lf\n", tempAmbiente->get_temperatura());
+	printf("A temperatura referencia eh: %.4lf\n", uart->get_temperaturaReferencia());
+	printf("A temperatura interna    eh: %.4lf\n", uart->get_temperaturaInterna());
 
 	lcd->set_mensagemAcima16(temperaturaAmbiente);
 	lcd->typeFloat( (float)(tempAmbiente->get_temperatura()) );
@@ -103,12 +103,12 @@ int main() {
 
 	lcd->mostrarMensagem();
 
-	// sleep(3); 
+	sleep(3); 
 
-	// lcd->set_mensagemAcima16(tempInterna);
-	// lcd->typeFloat( teste->get_temperaturaInterna());
+	lcd->set_mensagemAcima16(tempInterna);
+	lcd->typeFloat( uart->get_temperaturaInterna());
 
-	// lcd->mostrarMensagem();
+	lcd->mostrarMensagem();
 
 	// teste->enviarDisplayEstadoSistema( 0x00 );
 	// teste->enviarDisplayControle( 0x00 );

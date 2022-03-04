3c296769d2dc5b935bc673271e6c1d2f9e2b440b
brunocmo <brunocmo.nunes@gmail.com>
2022-03-04 21:11:59 +0000

    change timing

--- src/CommsProtocol.cpp
+++ src/CommsProtocol.cpp
@@ -226,7 +226,7 @@ bool CommsProtocol::enviarInformacao(int numeroCaracteres) {
         printf("Erro no envio de dados - TX\n");
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
     if( palavraDeEnvio[2] == 0xD1 || palavraDeEnvio[2] == 0xD2 ) 
         return EXIT_SUCCESS;

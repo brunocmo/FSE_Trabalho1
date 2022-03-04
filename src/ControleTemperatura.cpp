048f65ea206e91d4ed631709be9c11dd9d82d6e6
brunocmo <brunocmo.nunes@gmail.com>
2022-03-04 18:28:10 +0000

    small commit

--- src/ControleTemperatura.cpp
+++ src/ControleTemperatura.cpp
@@ -4,13 +4,13 @@ ControleTemperatura::ControleTemperatura() {
 
     if ( wiringPiSetup() == -1) exit(1);
 
-    printf("Criação do PWM \n");
+    printf("Criação do PWM 100 \n");
 
     softPwmCreate( 4, 0, 100);
 
     sleep(5);
 
-    printf("Mudou para 50% \n");
+    printf("Mudou para 50 \n");
     softPwmWrite( 4, 50);
 
     sleep(5);

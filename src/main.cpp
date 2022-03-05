259cc6c5d0c5325e3da0a31cc59e5b1478ed7b61
brunocmo <brunocmo.nunes@gmail.com>
2022-03-05 00:08:49 +0000

    preguiça

--- src/main.cpp
+++ src/main.cpp
@@ -109,8 +109,8 @@ int main() {
 
 	sleep(3); 
 
-	strcpy( tempInterna, "TI              ");
-	strcpy( tempReferencia, "TE    xD        ");
+	strcpy( tempInterna, "TI            ");
+	strcpy( tempReferencia, "TE  xD        ");
 
 	lcd->set_mensagemAcima16(tempInterna);
 	lcd->set_mensagemAbaixo16( tempReferencia );
